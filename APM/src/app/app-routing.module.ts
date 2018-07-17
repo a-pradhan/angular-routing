b572088151e253c44f6ad289d8b122b9a448cb89
Aditya Pradhan <apradhan@live.co.uk>
2018-07-18 00:25:47 +0100

    app component routes added to const

--- APM/src/app/app-routing.module.ts
+++ APM/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
+const ROUTES = [
+    { path: 'welcome', component: WelcomeComponent},
+    { path: '', redirectTo: 'welcome', pathMatch:'full'},
+    { path: '**', component: PageNotFoundComponent }
+  ];
+
 @NgModule({
     imports: [
-        RouterModule.forRoot([
-            { path: 'welcome', component: WelcomeComponent},
-            { path: '', redirectTo: 'welcome', pathMatch:'full'},
-            { path: '**', component: PageNotFoundComponent }
-          ])
+        RouterModule.forRoot(ROUTES)
     ],
     exports: [ RouterModule ]
 })
