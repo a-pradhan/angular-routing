8f40e38f6178edba2a1307c4ad291070e8010df2
Aditya Pradhan <apradhan@live.co.uk>
2018-07-19 00:59:41 +0100

    configured routing to product detail and product edit components in templates

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
     templateUrl: './app/products/product-detail.component.html'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
     pageTitle: string = 'Product Detail';
     product: IProduct;
     errorMessage: string;
 
-    constructor(private productService: ProductService) { }
+    constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
     getProduct(id: number) {
         this.productService.getProduct(id).subscribe(
             product => this.product = product,
             error => this.errorMessage = <any>error);
+    }
+
+    ngOnInit(): void {
+        let id = +this.route.snapshot.params['id'];
+        this.getProduct(id);
     }
 }
