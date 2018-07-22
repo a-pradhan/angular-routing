1b22c49b1f1a799ac7ed00c4c5da65d4afa623b7
Aditya Pradhan <apradhan@live.co.uk>
2018-07-22 23:22:23 +0100

    using observer to listen for changes in URL segments@

--- APM/src/app/products/product-edit.component.ts
+++ APM/src/app/products/product-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { MessageService } from '../messages/message.service';
 
@@ -18,7 +18,8 @@ export class ProductEditComponent implements OnInit {
 
     constructor(private productService: ProductService,
                 private messageService: MessageService,
-                private route: ActivatedRoute) { }
+                private route: ActivatedRoute,
+                private router: Router) { }
 
     getProduct(id: number): void {
         this.productService.getProduct(id)
@@ -71,10 +72,18 @@ export class ProductEditComponent implements OnInit {
         }
 
         // Navigate back to the product list
+        this.router.navigate(['/products'])
     }
 
     ngOnInit(): void {
-        let id = +this.route.snapshot.params['id'];
-        this.getProduct(id);
+        // let id = +this.route.snapshot.params['id'];
+        // this.getProduct(id);
+
+        this.route.params.subscribe(
+            params => {
+                let id = +params['id'];
+                this.getProduct(id);
+            }
+        )
     }
 }
